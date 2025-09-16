75a4254bbc8d4383d7a5a498ed0a9a45cf185e4f
goodmane6118-droid <goodmane6118@student.faytechcc.edu>
2025-09-16 10:15:59 -0400

    Add files via upload

---
+++ P1LAB1_GoodmanElijah.py
@@ -0,0 +1,18 @@
+# Elijah Goodman
+# 9/15/25
+# P1LAB1
+# Create a program promptimg the user input their first and last name and display it to them
+#
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter you last name: ")
+print()
+
+print()
+
+
+
+print("Hello,", first_name, last_name + "!", "Welcome to the CTI-110 course" + "!")
+print()
+
+
